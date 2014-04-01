b99f21ab009fcc03f072921a11daf77e38a20be8
Jonathan Novak <jnovak@gmail.com>
2014-04-01 12:48:19 -0700

    Fix k/v cascade. Make interfaces so that other libraries can loosely depend on health without an explicit dependency.

--- dbr.go
+++ dbr.go
@@ -1,6 +1,21 @@
-package main
+package dbr
 
 import (
 	"fmt"
-	"dbr"
 )
+
+type EventReceiver interface {
+	Event(eventName string)
+	EventKv(eventName string, kvs map[string]string)
+}
+
+type TimingReceiver interface {
+	Timing(eventName string, nanoseconds int64)
+	TimingKv(eventName string, nanoseconds int64, kvs map[string]string)
+}
+
+func DoSomething(s EventReceiver) {
+	fmt.Println("Doing it.", s)
+	
+	s.Event("sup")
+}
