84bd6d8483b9f8747d7cf87683f34150d78e78df
Jonathan Novak <jnovak@gmail.com>
2014-04-01 15:16:40 -0700

    Crap

---
+++ query.go
@@ -0,0 +1,14 @@
+package dbr
+// 
+// type Query struct {
+//     SelectSql     string
+//     TableNameSql  string
+//     WhereSql      string
+//     Params        []interface{}
+//     OrderSql      string
+//     LimitCount    uint32
+//     LimitValid    bool
+//     OffsetCount   uint64
+//     OffsetValid   bool
+// }
+// 
--- session.go
+++ session.go
@@ -12,3 +12,21 @@ func (cxn *Connection) NewSession(log EventReceiver) *Session {
 	return &Session{cxn: cxn, log: log}
 }
 
+// target can be:
+// - addr of a structure
+// - addr of slice of pointers to structures
+// - map of pointers to structures (addr of map also ok)
+// If it's a single structure, only the first record returned will be set.
+// If it's a slice or map, the slice/map won't be emptied first. New records will be allocated for each found record.
+// If its a map, there is the potential to overwrite values (keys are 'id')
+func (sess *Session) FindBySql(sql string, target interface{}) error {
+	// validate target
+	
+	sess.cxn.Db.Query()
+	
+	return nil
+}
+
+
+
+
