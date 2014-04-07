c4d7754eef229779cf97a75a7eedefa4a8be7122
Jonathan Novak <jnovak@gmail.com>
2014-04-07 15:18:32 -0700

    Moar

--- types.go
+++ types.go
@@ -9,9 +9,21 @@ type NullString struct {
 	sql.NullString
 }
 
-func (ns *NullString) MarshalJSON() ([]byte, error) {
-	if ns.Valid {
-		j, e := json.Marshal(ns.String)
+type NullInt64 struct {
+	sql.NullInt64
+}
+
+func (n *NullString) MarshalJSON() ([]byte, error) {
+	if n.Valid {
+		j, e := json.Marshal(n.String)
+		return j, e
+	}
+	return []byte("null"), nil
+}
+
+func (n *NullInt64) MarshalJSON() ([]byte, error) {
+	if n.Valid {
+		j, e := json.Marshal(n.Int64)
 		return j, e
 	}
 	return []byte("null"), nil
