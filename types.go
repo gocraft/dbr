b34a5c78c77211afe6143fae180a97daa6f4ce3f
Jonathan Novak <jnovak@gmail.com>
2014-04-09 14:34:28 -0700

    Timestamps

--- types.go
+++ types.go
@@ -3,6 +3,7 @@ package dbr
 import (
 	"database/sql"
 	"encoding/json"
+	"github.com/go-sql-driver/mysql"
 )
 
 type NullString struct {
@@ -13,12 +14,18 @@ type NullInt64 struct {
 	sql.NullInt64
 }
 
+type NullTime struct {
+	mysql.NullTime
+}
+
+var nullString = []byte("null")
+
 func (n *NullString) MarshalJSON() ([]byte, error) {
 	if n.Valid {
 		j, e := json.Marshal(n.String)
 		return j, e
 	}
-	return []byte("null"), nil
+	return nullString, nil
 }
 
 func (n *NullInt64) MarshalJSON() ([]byte, error) {
@@ -26,5 +33,13 @@ func (n *NullInt64) MarshalJSON() ([]byte, error) {
 		j, e := json.Marshal(n.Int64)
 		return j, e
 	}
-	return []byte("null"), nil
+	return nullString, nil
+}
+
+func (n *NullTime) MarshalJSON() ([]byte, error) {
+	if n.Valid {
+		j, e := json.Marshal(n.Time)
+		return j, e
+	}
+	return nullString, nil
 }
