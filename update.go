d2f6f175ffe82766f9b42f001f03c1747f8b6bf1
Tyler Smith <tyler@uservoice.com>
2014-05-24 22:01:24 -0700

    add UpdateBuilder + builder methods

---
+++ update.go
@@ -0,0 +1,162 @@
+package dbr
+
+import (
+  "bytes"
+  "fmt"
+  "database/sql"
+  "time"
+)
+
+type UpdateBuilder struct {
+  *Session
+  runner
+
+  Table           string
+  SetClauses      []*setClause
+  WhereFragments  []*whereFragment
+  OrderBys        []string
+  LimitCount      uint64
+  LimitValid      bool
+  OffsetCount     uint64
+  OffsetValid     bool
+}
+
+type setClause struct {
+  column string
+  value  interface{}
+}
+
+func (sess *Session) Update(table string) *UpdateBuilder {
+  return &UpdateBuilder{
+    Session: sess,
+    runner:  sess.cxn.Db,
+    Table:   table,
+  }
+}
+
+func (tx *Tx) Update(table string) *UpdateBuilder {
+  return &UpdateBuilder{
+    Session: tx.Session,
+    runner:  tx.Tx,
+    Table:   table,
+  }
+}
+
+func (b *UpdateBuilder) Set(column string, value interface{}) *UpdateBuilder {
+  b.SetClauses = append(b.SetClauses, &setClause{column: column, value: value})
+  return b
+}
+
+func (b *UpdateBuilder) SetMap(clauses map[string]interface{}) *UpdateBuilder {
+  for col, val := range clauses {
+    b = b.Set(col, val)
+  }
+  return b
+}
+
+func (b *UpdateBuilder) Where(whereSqlOrMap interface{}, args ...interface{}) *UpdateBuilder {
+  b.WhereFragments = append(b.WhereFragments, newWhereFragment(whereSqlOrMap, args))
+  return b
+}
+
+func (b *UpdateBuilder) OrderBy(ord string) *UpdateBuilder {
+  b.OrderBys = append(b.OrderBys, ord)
+  return b
+}
+
+func (b *UpdateBuilder) OrderDir(ord string, isAsc bool) *UpdateBuilder {
+  if isAsc {
+    b.OrderBys = append(b.OrderBys, ord+" ASC")
+  } else {
+    b.OrderBys = append(b.OrderBys, ord+" DESC")
+  }
+  return b
+}
+
+func (b *UpdateBuilder) Limit(limit uint64) *UpdateBuilder {
+  b.LimitCount = limit
+  b.LimitValid = true
+  return b
+}
+
+func (b *UpdateBuilder) Offset(offset uint64) *UpdateBuilder {
+  b.OffsetCount = offset
+  b.OffsetValid = true
+  return b
+}
+
+func (b *UpdateBuilder) ToSql() (string, []interface{}) {
+  if len(b.Table) == 0 {
+    panic("no table specified")
+  }
+  if len(b.SetClauses) == 0 {
+    panic("no set clauses specified")
+  }
+
+  var sql bytes.Buffer
+  var args []interface{}
+
+  sql.WriteString("UPDATE ")
+  sql.WriteString(b.Table)
+  sql.WriteString(" SET ")
+
+  // Build SET clause SQL with placeholders and add values to args
+  for i, c := range b.SetClauses {
+    if i > 0 {
+      sql.WriteString(", ")
+    }
+    sql.WriteString(c.column)
+    sql.WriteString(" = ?")
+
+    args = append(args, c.value)
+  }
+
+  // Write WHERE clause if we have any fragments
+  if len(b.WhereFragments) > 0 {
+    sql.WriteString(" WHERE ")
+    writeWhereFragmentsToSql(b.WhereFragments, &sql, &args)
+  }
+
+  // Ordering and limiting
+  if len(b.OrderBys) > 0 {
+    sql.WriteString(" ORDER BY ")
+    for i, s := range b.OrderBys {
+      if i > 0 {
+        sql.WriteString(", ")
+      }
+      sql.WriteString(s)
+    }
+  }
+
+  if b.LimitValid {
+    sql.WriteString(" LIMIT ")
+    fmt.Fprint(&sql, b.LimitCount)
+  }
+
+  if b.OffsetValid {
+    sql.WriteString(" OFFSET ")
+    fmt.Fprint(&sql, b.OffsetCount)
+  }
+
+  return sql.String(), args
+}
+
+func (b *UpdateBuilder) Exec() (sql.Result, error) {
+  sql, args := b.ToSql()
+
+  fullSql, err := Interpolate(sql, args)
+  if err != nil {
+    return nil, b.EventErrKv("dbr.update.exec.interpolate", err, kvs{"sql": fullSql})
+  }
+
+  // Start the timer:
+  startTime := time.Now()
+  defer func() { b.TimingKv("dbr.update", time.Since(startTime).Nanoseconds(), kvs{"sql": fullSql}) }()
+
+  result, err := b.runner.Exec(fullSql)
+  if err != nil {
+    return result, b.EventErrKv("dbr.update.exec.exec", err, kvs{"sql": fullSql})
+  }
+
+  return result, nil
+}
---
+++ update_test.go
@@ -0,0 +1,88 @@
+package dbr
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func BenchmarkUpdateValuesSql(b *testing.B) {
+  s := createFakeSession()
+
+  b.ResetTimer()
+
+  for i := 0; i < b.N; i++ {
+    s.Update("alpha").Set("something_id", 1).Where("id", 1).ToSql()
+  }
+}
+
+func BenchmarkUpdateValueMapSql(b *testing.B) {
+  s := createFakeSession()
+
+  b.ResetTimer()
+
+  for i := 0; i < b.N; i++ {
+    s.Update("alpha").Set("something_id", 1).SetMap(map[string]interface{}{"b": 1, "c": 2}).Where("id", 1).ToSql()
+  }
+}
+
+func TestUpdateAllToSql(t *testing.T) {
+  s := createFakeSession()
+
+  sql, args := s.Update("a").Set("b", 1).Set("c", 2).ToSql()
+
+  assert.Equal(t, sql, "UPDATE a SET b = ?, c = ?")
+  assert.Equal(t, args, []interface{}{1, 2})
+}
+
+func TestUpdateSingleToSql(t *testing.T) {
+  s := createFakeSession()
+
+  sql, args := s.Update("a").Set("b", 1).Set("c", 2).Where("id = ?", 1).ToSql()
+
+  assert.Equal(t, sql, "UPDATE a SET b = ?, c = ? WHERE (id = ?)")
+  assert.Equal(t, args, []interface{}{1, 2, 1})
+}
+
+func TestUpdateSetMapToSql(t *testing.T) {
+  s := createFakeSession()
+
+  sql, args := s.Update("a").SetMap(map[string]interface{}{"b": 1, "c": 2}).Where("id = ?", 1).ToSql()
+
+  assert.Equal(t, sql, "UPDATE a SET b = ?, c = ? WHERE (id = ?)")
+  assert.Equal(t, args, []interface{}{1, 2, 1})
+}
+
+func TestUpdateTenStaringFromTwentyToSql(t *testing.T) {
+  s := createFakeSession()
+
+  sql, args := s.Update("a").Set("b", 1).Limit(10).Offset(20).ToSql()
+
+  assert.Equal(t, sql, "UPDATE a SET b = ? LIMIT 10 OFFSET 20")
+  assert.Equal(t, args, []interface{}{1})
+}
+
+func TestUpdateReal(t *testing.T) {
+  s := createRealSessionWithFixtures()
+
+  // Insert a George
+  res, err := s.InsertInto("dbr_people").Columns("name", "email").Values("George", "george@whitehouse.gov").Exec()
+  assert.NoError(t, err)
+
+  // Get George's ID
+  id, err := res.LastInsertId()
+  assert.NoError(t, err)
+
+  // Rename our George to Barack
+  res, err = s.Update("dbr_people").SetMap(map[string]interface{}{"name": "Barack", "email": "barack@whitehouse.gov"}).Where("id = ?", id).Exec()
+
+  assert.NoError(t, err)
+
+  var person dbrPerson
+  err = s.Select("*").From("dbr_people").Where("id = ?", id).LoadOne(&person)
+  assert.NoError(t, err)
+
+  assert.Equal(t, person.Id, id)
+  assert.Equal(t, person.Name, "Barack")
+  assert.Equal(t, person.Email.Valid, true)
+  assert.Equal(t, person.Email.String, "barack@whitehouse.gov")
+}
