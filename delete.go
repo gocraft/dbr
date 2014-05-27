7f0f62b8b7708ae60c3d712a3641d4a98b060d54
Tyler Smith <tyler@uservoice.com>
2014-05-27 12:08:10 -0700

    go fmt things

--- delete.go
+++ delete.go
@@ -1,129 +1,129 @@
 package dbr
 
 import (
-  "bytes"
-  "fmt"
-  "database/sql"
-  "time"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"time"
 )
 
 type DeleteBuilder struct {
-  *Session
-  runner
+	*Session
+	runner
 
-  From string
-  WhereFragments  []*whereFragment
-  OrderBys        []string
-  LimitCount      uint64
-  LimitValid      bool
-  OffsetCount     uint64
-  OffsetValid     bool
+	From           string
+	WhereFragments []*whereFragment
+	OrderBys       []string
+	LimitCount     uint64
+	LimitValid     bool
+	OffsetCount    uint64
+	OffsetValid    bool
 }
 
 func (sess *Session) DeleteFrom(from string) *DeleteBuilder {
-  return &DeleteBuilder{
-    Session: sess,
-    runner:  sess.cxn.Db,
-    From:    from,
-  }
+	return &DeleteBuilder{
+		Session: sess,
+		runner:  sess.cxn.Db,
+		From:    from,
+	}
 }
 
 func (tx *Tx) DeleteFrom(from string) *DeleteBuilder {
-  return &DeleteBuilder{
-    Session: tx.Session,
-    runner:  tx.Tx,
-    From:    from,
-  }
+	return &DeleteBuilder{
+		Session: tx.Session,
+		runner:  tx.Tx,
+		From:    from,
+	}
 }
 
 func (b *DeleteBuilder) Where(whereSqlOrMap interface{}, args ...interface{}) *DeleteBuilder {
-  b.WhereFragments = append(b.WhereFragments, newWhereFragment(whereSqlOrMap, args))
-  return b
+	b.WhereFragments = append(b.WhereFragments, newWhereFragment(whereSqlOrMap, args))
+	return b
 }
 
 func (b *DeleteBuilder) OrderBy(ord string) *DeleteBuilder {
-  b.OrderBys = append(b.OrderBys, ord)
-  return b
+	b.OrderBys = append(b.OrderBys, ord)
+	return b
 }
 
 func (b *DeleteBuilder) OrderDir(ord string, isAsc bool) *DeleteBuilder {
-  if isAsc {
-    b.OrderBys = append(b.OrderBys, ord+" ASC")
-  } else {
-    b.OrderBys = append(b.OrderBys, ord+" DESC")
-  }
-  return b
+	if isAsc {
+		b.OrderBys = append(b.OrderBys, ord+" ASC")
+	} else {
+		b.OrderBys = append(b.OrderBys, ord+" DESC")
+	}
+	return b
 }
 
 func (b *DeleteBuilder) Limit(limit uint64) *DeleteBuilder {
-  b.LimitCount = limit
-  b.LimitValid = true
-  return b
+	b.LimitCount = limit
+	b.LimitValid = true
+	return b
 }
 
 func (b *DeleteBuilder) Offset(offset uint64) *DeleteBuilder {
-  b.OffsetCount = offset
-  b.OffsetValid = true
-  return b
+	b.OffsetCount = offset
+	b.OffsetValid = true
+	return b
 }
 
 func (b *DeleteBuilder) ToSql() (string, []interface{}) {
-  if len(b.From) == 0 {
-    panic("no table specified")
-  }
+	if len(b.From) == 0 {
+		panic("no table specified")
+	}
 
-  var sql bytes.Buffer
-  var args []interface{}
+	var sql bytes.Buffer
+	var args []interface{}
 
-  sql.WriteString("DELETE FROM ")
-  sql.WriteString(b.From)
+	sql.WriteString("DELETE FROM ")
+	sql.WriteString(b.From)
 
-  // Write WHERE clause if we have any fragments
-  if len(b.WhereFragments) > 0 {
-    sql.WriteString(" WHERE ")
-    writeWhereFragmentsToSql(b.WhereFragments, &sql, &args)
-  }
+	// Write WHERE clause if we have any fragments
+	if len(b.WhereFragments) > 0 {
+		sql.WriteString(" WHERE ")
+		writeWhereFragmentsToSql(b.WhereFragments, &sql, &args)
+	}
 
-  // Ordering and limiting
-  if len(b.OrderBys) > 0 {
-    sql.WriteString(" ORDER BY ")
-    for i, s := range b.OrderBys {
-      if i > 0 {
-        sql.WriteString(", ")
-      }
-      sql.WriteString(s)
-    }
-  }
+	// Ordering and limiting
+	if len(b.OrderBys) > 0 {
+		sql.WriteString(" ORDER BY ")
+		for i, s := range b.OrderBys {
+			if i > 0 {
+				sql.WriteString(", ")
+			}
+			sql.WriteString(s)
+		}
+	}
 
-  if b.LimitValid {
-    sql.WriteString(" LIMIT ")
-    fmt.Fprint(&sql, b.LimitCount)
-  }
+	if b.LimitValid {
+		sql.WriteString(" LIMIT ")
+		fmt.Fprint(&sql, b.LimitCount)
+	}
 
-  if b.OffsetValid {
-    sql.WriteString(" OFFSET ")
-    fmt.Fprint(&sql, b.OffsetCount)
-  }
+	if b.OffsetValid {
+		sql.WriteString(" OFFSET ")
+		fmt.Fprint(&sql, b.OffsetCount)
+	}
 
-  return sql.String(), args
+	return sql.String(), args
 }
 
 func (b *DeleteBuilder) Exec() (sql.Result, error) {
-  sql, args := b.ToSql()
+	sql, args := b.ToSql()
 
-  fullSql, err := Interpolate(sql, args)
-  if err != nil {
-    return nil, b.EventErrKv("dbr.delete.exec.interpolate", err, kvs{"sql": fullSql})
-  }
+	fullSql, err := Interpolate(sql, args)
+	if err != nil {
+		return nil, b.EventErrKv("dbr.delete.exec.interpolate", err, kvs{"sql": fullSql})
+	}
 
-  // Start the timer:
-  startTime := time.Now()
-  defer func() { b.TimingKv("dbr.delete", time.Since(startTime).Nanoseconds(), kvs{"sql": fullSql}) }()
+	// Start the timer:
+	startTime := time.Now()
+	defer func() { b.TimingKv("dbr.delete", time.Since(startTime).Nanoseconds(), kvs{"sql": fullSql}) }()
 
-  result, err := b.runner.Exec(fullSql)
-  if err != nil {
-    return result, b.EventErrKv("dbr.delete.exec.exec", err, kvs{"sql": fullSql})
-  }
+	result, err := b.runner.Exec(fullSql)
+	if err != nil {
+		return result, b.EventErrKv("dbr.delete.exec.exec", err, kvs{"sql": fullSql})
+	}
 
-  return result, nil
+	return result, nil
 }
--- delete_test.go
+++ delete_test.go
@@ -1,67 +1,67 @@
 package dbr
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func BenchmarkDeleteSql(b *testing.B) {
-  s := createFakeSession()
+	s := createFakeSession()
 
-  b.ResetTimer()
+	b.ResetTimer()
 
-  for i := 0; i < b.N; i++ {
-    s.DeleteFrom("alpha").Where("a", "b").Limit(1).OrderDir("id", true).ToSql()
-  }
+	for i := 0; i < b.N; i++ {
+		s.DeleteFrom("alpha").Where("a", "b").Limit(1).OrderDir("id", true).ToSql()
+	}
 }
 
 func TestDeleteAllToSql(t *testing.T) {
-  s := createFakeSession()
+	s := createFakeSession()
 
-  sql, _ := s.DeleteFrom("a").ToSql()
+	sql, _ := s.DeleteFrom("a").ToSql()
 
-  assert.Equal(t, sql, "DELETE FROM a")
+	assert.Equal(t, sql, "DELETE FROM a")
 }
 
 func TestDeleteSingleToSql(t *testing.T) {
-  s := createFakeSession()
+	s := createFakeSession()
 
-  sql, args := s.DeleteFrom("a").Where("id = ?", 1).ToSql()
+	sql, args := s.DeleteFrom("a").Where("id = ?", 1).ToSql()
 
-  assert.Equal(t, sql, "DELETE FROM a WHERE (id = ?)")
-  assert.Equal(t, args, []interface{}{1})
+	assert.Equal(t, sql, "DELETE FROM a WHERE (id = ?)")
+	assert.Equal(t, args, []interface{}{1})
 }
 
 func TestDeleteTenStaringFromTwentyToSql(t *testing.T) {
-  s := createFakeSession()
+	s := createFakeSession()
 
-  sql, _ := s.DeleteFrom("a").Limit(10).Offset(20).OrderBy("id").ToSql()
+	sql, _ := s.DeleteFrom("a").Limit(10).Offset(20).OrderBy("id").ToSql()
 
-  assert.Equal(t, sql, "DELETE FROM a ORDER BY id LIMIT 10 OFFSET 20")
+	assert.Equal(t, sql, "DELETE FROM a ORDER BY id LIMIT 10 OFFSET 20")
 }
 
 func TestDeleteReal(t *testing.T) {
-  s := createRealSessionWithFixtures()
+	s := createRealSessionWithFixtures()
 
-  // Insert a Barack
-  res, err := s.InsertInto("dbr_people").Columns("name", "email").Values("Barack", "barack@whitehouse.gov").Exec()
-  assert.NoError(t, err)
+	// Insert a Barack
+	res, err := s.InsertInto("dbr_people").Columns("name", "email").Values("Barack", "barack@whitehouse.gov").Exec()
+	assert.NoError(t, err)
 
-  // Get Barack's ID
-  id, err := res.LastInsertId()
-  assert.NoError(t, err)
+	// Get Barack's ID
+	id, err := res.LastInsertId()
+	assert.NoError(t, err)
 
-  // Delete Barack
-  res, err = s.DeleteFrom("dbr_people").Where("id = ?", id).Exec()
-  assert.NoError(t, err)
+	// Delete Barack
+	res, err = s.DeleteFrom("dbr_people").Where("id = ?", id).Exec()
+	assert.NoError(t, err)
 
-  // Ensure we only reflected one row and that the id no longer exists
-  rowsAff, err := res.RowsAffected()
-  assert.NoError(t, err)
-  assert.Equal(t, rowsAff, 1)
+	// Ensure we only reflected one row and that the id no longer exists
+	rowsAff, err := res.RowsAffected()
+	assert.NoError(t, err)
+	assert.Equal(t, rowsAff, 1)
 
-  var count int64
-  err = s.Select("count(*)").From("dbr_people").Where("id = ?", id).LoadValue(&count)
-  assert.NoError(t, err)
-  assert.Equal(t, count, 0)
+	var count int64
+	err = s.Select("count(*)").From("dbr_people").Where("id = ?", id).LoadValue(&count)
+	assert.NoError(t, err)
+	assert.Equal(t, count, 0)
 }
