089494d88ec8f59b7a7ff8fd56837f978abfd313
Tyler Smith <tyler@uservoice.com>
2014-09-16 11:15:31 -0700

    TWEAK: Remove map support from LoadAll because it's easily implemented.

--- select_load.go
+++ select_load.go
@@ -15,31 +15,31 @@ import (
 // dest can be:
 // - addr of a structure
 // - addr of slice of pointers to structures
-// - map of pointers to structures (addr of map also ok)
 // If it's a single structure, only the first record returned will be set.
-// If it's a slice or map, the slice/map won't be emptied first. New records will be allocated for each found record.
-// If its a map, there is the potential to overwrite values (keys are 'id')
+// If it's a slice it won't be emptied first. New records will be allocated for each found record.
 // Returns the number of items found (which is not necessarily the # of items set)
 func (b *SelectBuilder) LoadAll(dest interface{}) (int, error) {
 	//
 	// Validate the dest, and extract the reflection values we need.
 	//
-	valueOfDest := reflect.ValueOf(dest) // We want this to eventually be a map or slice
-	kindOfDest := valueOfDest.Kind()     // And this eventually needs to be a map or slice as well
 
-	if kindOfDest == reflect.Ptr {
-		valueOfDest = reflect.Indirect(valueOfDest)
-		kindOfDest = valueOfDest.Kind()
-	} else if kindOfDest == reflect.Map {
-		// we're good
-	} else {
-		panic("invalid type passed to LoadAll. Need a map or addr of slice")
+	// This must be a pointer to a slice
+	valueOfDest := reflect.ValueOf(dest)
+	kindOfDest := valueOfDest.Kind()
+
+	if kindOfDest != reflect.Ptr {
+		panic("invalid type passed to LoadAll. Need a pointer to a slice")
 	}
 
-	if !(kindOfDest == reflect.Map || kindOfDest == reflect.Slice) {
-		panic("invalid type passed to LoadAll. Need a map or addr of slice")
+	// This must a slice
+	valueOfDest = reflect.Indirect(valueOfDest)
+	kindOfDest = valueOfDest.Kind()
+
+	if kindOfDest != reflect.Slice {
+		panic("invalid type passed to LoadAll. Need a pointer to a slice")
 	}
 
+	// The slice elements must be pointers to structures
 	recordType := valueOfDest.Type().Elem()
 	if recordType.Kind() != reflect.Ptr {
 		panic("Elements need to be pointers to structures")
@@ -84,34 +84,30 @@ func (b *SelectBuilder) LoadAll(dest interface{}) (int, error) {
 	}
 
 	// Iterate over rows
-	if kindOfDest == reflect.Slice {
-		sliceValue := valueOfDest
-		for rows.Next() {
-			// Create a new record to store our row:
-			pointerToNewRecord := reflect.New(recordType)
-			newRecord := reflect.Indirect(pointerToNewRecord)
+	sliceValue := valueOfDest
+	for rows.Next() {
+		// Create a new record to store our row:
+		pointerToNewRecord := reflect.New(recordType)
+		newRecord := reflect.Indirect(pointerToNewRecord)
 
-			// Build a 'holder', which is an []interface{}. Each value will be the address of the field corresponding to our newly made record:
-			holder, err := b.holderFor(newRecord, fieldMap)
-			if err != nil {
-				return numberOfRowsReturned, b.EventErrKv("dbr.select.load_all.holderFor", err, kvs{"sql": fullSql})
-			}
-
-			// Load up our new structure with the row's values
-			err = rows.Scan(holder...)
-			if err != nil {
-				return numberOfRowsReturned, b.EventErrKv("dbr.select.load_all.scan", err, kvs{"sql": fullSql})
-			}
-
-			// Append our new record to the slice:
-			sliceValue = reflect.Append(sliceValue, pointerToNewRecord)
-
-			numberOfRowsReturned += 1
+		// Build a 'holder', which is an []interface{}. Each value will be the address of the field corresponding to our newly made record:
+		holder, err := b.holderFor(newRecord, fieldMap)
+		if err != nil {
+			return numberOfRowsReturned, b.EventErrKv("dbr.select.load_all.holderFor", err, kvs{"sql": fullSql})
 		}
-		valueOfDest.Set(sliceValue)
-	} else { // Map
 
+		// Load up our new structure with the row's values
+		err = rows.Scan(holder...)
+		if err != nil {
+			return numberOfRowsReturned, b.EventErrKv("dbr.select.load_all.scan", err, kvs{"sql": fullSql})
+		}
+
+		// Append our new record to the slice:
+		sliceValue = reflect.Append(sliceValue, pointerToNewRecord)
+
+		numberOfRowsReturned += 1
 	}
+	valueOfDest.Set(sliceValue)
 
 	// Check for errors at the end. Supposedly these are error that can happen during iteration.
 	if err = rows.Err(); err != nil {
