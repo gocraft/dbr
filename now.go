a1d8e8871993fb13c85f6ff06c955d2e469abe50
taylorchu <tailinchu@gmail.com>
2016-08-02 12:10:31 -0700

    TWEAK: make time format const

--- now.go
+++ now.go
@@ -5,11 +5,12 @@ import (
 	"time"
 )
 
-type nowSentinel struct{}
-
 // Now is a value that serializes to the current time
 var Now = nowSentinel{}
-var timeFormat = "2006-01-02 15:04:05.000000"
+
+const timeFormat = "2006-01-02 15:04:05.000000"
+
+type nowSentinel struct{}
 
 // Value implements a valuer for compatibility
 func (n nowSentinel) Value() (driver.Value, error) {
