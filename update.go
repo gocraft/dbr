264828106a453eb462a4894d7312bd04de97ee25
Earncef Sequeira <earncef@earncef.com>
2018-05-09 13:46:51 +0200

    Support for struct (Record) in Update

--- update.go
+++ update.go
@@ -3,6 +3,7 @@ package dbr
 import (
 	"context"
 	"database/sql"
+	"reflect"
 	"strconv"
 )
 
@@ -16,6 +17,7 @@ type UpdateStmt struct {
 
 	Table      string
 	Value      map[string]interface{}
+	Column     []string
 	WhereCond  []Builder
 	LimitCount int64
 }
@@ -158,4 +160,25 @@ func (b *UpdateStmt) Exec() (sql.Result, error) {
 
 func (b *UpdateStmt) ExecContext(ctx context.Context) (sql.Result, error) {
 	return exec(ctx, b.runner, b.EventReceiver, b, b.Dialect)
+}
+
+// Columns adds columns
+func (b *UpdateStmt) Columns(column ...string) *UpdateStmt {
+	b.Column = column
+	return b
+}
+
+// Record adds a tuple for columns from a struct
+func (b *UpdateStmt) Record(structValue interface{}) *UpdateStmt {
+	v := reflect.Indirect(reflect.ValueOf(structValue))
+
+	if v.Kind() == reflect.Struct {
+		m := structMap(v)
+		for _, col := range b.Column {
+			if val, ok := m[col]; ok {
+				b.Set(col, val.Interface())
+			}
+		}
+	}
+	return b
 }
--- update_test.go
+++ update_test.go
@@ -7,14 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type updateTest struct {
+	C string `db:"b"`
+	D string `db:"c"`
+}
+
 func TestUpdateStmt(t *testing.T) {
 	buf := NewBuffer()
-	builder := Update("table").Set("a", 1).Where(Eq("b", 2))
+	builder := Update("table").Set("a", 1).Columns([]string{"b"}...).Record(&updateTest{
+		C: "two",
+		D: "three",
+	}).Where(Eq("b", 2))
 	err := builder.Build(dialect.MySQL, buf)
 	assert.NoError(t, err)
 
-	assert.Equal(t, "UPDATE `table` SET `a` = ? WHERE (`b` = ?)", buf.String())
-	assert.Equal(t, []interface{}{1, 2}, buf.Value())
+	assert.Equal(t, "UPDATE `table` SET `a` = ?, `b` = ? WHERE (`b` = ?)", buf.String())
+	assert.Equal(t, []interface{}{1, "two", 2}, buf.Value())
 }
 
 func BenchmarkUpdateValuesSQL(b *testing.B) {
@@ -28,5 +36,14 @@ func BenchmarkUpdateMapSQL(b *testing.B) {
 	buf := NewBuffer()
 	for i := 0; i < b.N; i++ {
 		Update("table").SetMap(map[string]interface{}{"a": 1, "b": 2}).Build(dialect.MySQL, buf)
+	}
+}
+
+func BenchmarkUpdateRecordSQL(b *testing.B) {
+	buf := NewBuffer()
+	for i := 0; i < b.N; i++ {
+		Update("table").Record(&updateTest{
+			C: "two",
+		}).Build(dialect.MySQL, buf)
 	}
 }
