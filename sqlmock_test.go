6d7a1fb7fd6afd85dcd0e537046f78a233e48a6d
taylorchu <tailinchu@gmail.com>
2018-05-09 22:39:41 -0700

    DOC: show a test for sqlmock

---
+++ sqlmock_test.go
@@ -0,0 +1,33 @@
+package dbr
+
+import (
+	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/gocraft/dbr/dialect"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSQLMock(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	conn := &Connection{
+		DB:            db,
+		EventReceiver: &NullEventReceiver{},
+		Dialect:       dialect.MySQL,
+	}
+	sess := conn.NewSession(nil)
+
+	mock.ExpectQuery("SELECT id FROM suggestions").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1).AddRow(2))
+	id, err := sess.Select("id").From("suggestions").ReturnInt64s()
+	require.NoError(t, err)
+	require.Equal(t, []int64{1, 2}, id)
+
+	mock.ExpectClose()
+	conn.Close()
+
+	require.NoError(t, mock.ExpectationsWereMet())
+}
