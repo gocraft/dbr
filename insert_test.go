3f07b54eb971728c16d7579647cab17a701680a7
Nick Bulischeck <nbulischeck@users.noreply.github.com>
2019-10-26 20:16:22 -0500

    DBR-193 Add ignore option to insert statements (#194)

--- insert_test.go
+++ insert_test.go
@@ -14,13 +14,13 @@ type insertTest struct {
 
 func TestInsertStmt(t *testing.T) {
 	buf := NewBuffer()
-	builder := InsertInto("table").Columns("a", "b").Values(1, "one").Record(&insertTest{
+	builder := InsertInto("table").Ignore().Columns("a", "b").Values(1, "one").Record(&insertTest{
 		A: 2,
 		C: "two",
 	}).Comment("INSERT TEST")
 	err := builder.Build(dialect.MySQL, buf)
 	require.NoError(t, err)
-	require.Equal(t, "/* INSERT TEST */\nINSERT INTO `table` (`a`,`b`) VALUES (?,?), (?,?)", buf.String())
+	require.Equal(t, "/* INSERT TEST */\nINSERT IGNORE INTO `table` (`a`,`b`) VALUES (?,?), (?,?)", buf.String())
 	require.Equal(t, []interface{}{1, "one", 2, "two"}, buf.Value())
 }
 
